6541b09347f1d2957b5f79157ee56394e6b44520
pori0000 <pori0000@hanmail.net>
2019-09-27 16:31:16 +0900

    signinAppbar
    signinAppbar

--- Looquiz/app/src/main/java/com/example/looquiz/SignInActivity.kt
+++ Looquiz/app/src/main/java/com/example/looquiz/SignInActivity.kt
@@ -1,10 +1,13 @@
 package com.example.looquiz
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.AsyncTask
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.act_signin.*
 import okhttp3.OkHttpClient
@@ -15,6 +18,18 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_signin)
+
+        val view = window.decorView
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (view != null) {
+                // 23 버전 이상일 때 상태바 하얀 색상에 회색 아이콘 색상을 설정
+                view.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                window.statusBarColor = Color.parseColor("#FFFFFF")
+            }
+        } else if (Build.VERSION.SDK_INT >= 21) {
+            // 21 버전 이상일 때
+            window.statusBarColor = Color.BLACK
+        }
 
         signin_checkbox.setOnClickListener { SharedClass(applicationContext).setAuto(signin_checkbox.isChecked) }
         if(SharedClass(applicationContext).getAuto() && !SharedClass(applicationContext).getToken().isNullOrEmpty())
